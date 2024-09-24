33a65250c37fd70a3fe3b34adb8954a07b1a1572
Kobayashi82 <kobayashi82@outlook.com>
2024-09-24 23:51:30 +0200

    Added some files

---
+++ bin/index.py
@@ -0,0 +1,7 @@
+
+print("Content-Type: text/html\r\n\r\n")
+
+print("<html><body>")
+print("<h1>Hola, mundo desde Python CGI!</h1>")
+print("<p>Este es un mensaje de prueba desde un script Python CGI.</p>")
+print("</body></html>")
